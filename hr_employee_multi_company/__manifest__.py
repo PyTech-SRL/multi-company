c035f5e36f5aaec8ec15068b8a37d167ff2603f3
Samir GUESMI <samir.guesmi@acsone.eu>
2025-07-01 10:31:07 +0200

    [IMP] hr_employee_multi_company: pre-commit auto fixes

--- hr_employee_multi_company/__manifest__.py
+++ hr_employee_multi_company/__manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'HR Employee Multi Company',
-    'summary': """
+    "name": "HR Employee Multi Company",
+    "summary": """
         This module add multi-company management to HR Employee""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,' 'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/multi-company',
-    'depends': ['hr'],
-    'data': ['security/hr_employee.xml'],
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/multi-company",
+    "depends": ["hr"],
+    "data": ["security/hr_employee.xml"],
 }
