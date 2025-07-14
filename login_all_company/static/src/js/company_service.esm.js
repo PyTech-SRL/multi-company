3465602931772f26898d8c8964d1719af60c0cbf
Enric Tobella <enric.tobella@dixmit.com>
2025-07-14 06:34:15 +0200

    [MIG] login_all_company: Migration to 18.0

--- login_all_company/static/src/js/company_service.esm.js
+++ login_all_company/static/src/js/company_service.esm.js
@@ -1,20 +1,21 @@
-/** @odoo-module **/
-
 import {companyService} from "@web/webclient/company_service";
-import {patch} from "web.utils";
+import {cookie} from "@web/core/browser/cookie";
+import {patch} from "@web/core/utils/patch";
 import {session} from "@web/session";
 
-patch(companyService, "login_all_company/static/src/js/company_service.esm.js", {
-    start(env, {router, cookie}) {
-        if (!cookie.current.cids && !router.current.hash.cids) {
-            var companies = [session.company_id];
-            _.forEach(session.user_companies.allowed_companies, function (company) {
-                if (company.id !== session.company_id) {
+patch(companyService, {
+    start() {
+        if (cookie && !cookie.get("cids")) {
+            var companies = [session.user_companies.current_company];
+            for (const company of Object.values(
+                session.user_companies.allowed_companies
+            )) {
+                if (company.id !== session.user_companies.current_company) {
                     companies.push(company.id);
                 }
-            });
-            cookie.setCookie("cids", companies.join(","));
+            }
+            cookie.set("cids", companies.join("-"));
         }
-        return this._super(...arguments);
+        return super.start(...arguments);
     },
 });
