d8baf1df79be97b0cc70c13eb0b3e4b33b861587
Simone Rubino <simone.rubino@pytech.it>
2026-09-03 16:07:41 +0200

    [MIG] company_dependent_flag: Migration to 18.0

--- web_field_company_dependent_flag/tests/test_module.py
+++ web_field_company_dependent_flag/tests/test_module.py
@@ -1,17 +1,17 @@
 # © 2023 David BEAL @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests import common
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class Test(common.TransactionCase):
+class Test(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.partner_view = cls.env.ref("base.view_partner_form")
 
     def test_class_company(self):
         arch, view = self.env["res.partner"]._get_view(view_id=self.partner_view.id)
-        for field in arch.xpath("//field[@name='phone']"):
+        for field in arch.xpath("//field[@name='barcode']"):
             self.assertIn("building", field.attrib.get("class"))
