edf574db4cf04d001b43f3bd2e8123574f0365b7
Simone Rubino <simone.rubino@pytech.it>
2026-09-03 16:07:41 +0200

    [MIG] company_dependent_flag: Migration to 18.0

--- web_field_company_dependent_flag/__init__.py
+++ web_field_company_dependent_flag/__init__.py
@@ -1,6 +1,1 @@
-from odoo.tools import config
-
 from . import models
-
-if not config.get("without_demo"):
-    from . import demo
--- web_field_company_dependent_flag/tests/test_module.py
+++ web_field_company_dependent_flag/tests/test_module.py
@@ -1,17 +1,36 @@
 # © 2023 David BEAL @ Akretion
+# Copyright 2026 Simone Rubino - PyTech
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests import common
+from odoo import fields, tests
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class Test(common.TransactionCase):
+@tests.tagged("-at_install", "post_install")
+class Test(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.partner_view = cls.env.ref("base.view_partner_form")
+        cls.partner_view.inherit_children_ids = [
+            fields.Command.create(
+                {
+                    "name": "Test view to include a company dependent field",
+                    "model": "res.partner",
+                    "arch": """
+<field name="vat" position="after">
+    <field name="barcode" />
+</field>
+            """,
+                }
+            )
+        ]
 
     def test_class_company(self):
         arch, view = self.env["res.partner"]._get_view(view_id=self.partner_view.id)
-        for field in arch.xpath("//field[@name='phone']"):
+        for field in arch.xpath("//field[@name='barcode']"):
             self.assertIn("building", field.attrib.get("class"))
+            break
+        else:
+            self.fail("Barcode field not found in the view")
