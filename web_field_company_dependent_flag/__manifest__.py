bc7111d1ddd11d5e7215fb4d1a328e563262fec5
Simone Rubino <simone.rubino@pytech.it>
2026-09-03 16:07:41 +0200

    [MIG] company_dependent_flag: Migration to 18.0

---
+++ web_field_company_dependent_flag/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Company Dependent Flag",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "summary": "Apply CSS style to company dependent fields",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "maintainer": ["bealdav"],
+    "website": "https://github.com/OCA/multi-company",
+    "category": "web",
+    "depends": [
+        "base",
+    ],
+}
